ee6e1ae2d0df7767c9157f991978a1a7302bdb10
Parashuram <parashurampawar56@gmail.com>
2026-02-07 21:19:33 +0530

    modify program: [parashuram] script to perform wordcount using builtin

--- scripts/wordcount_execution_inbuilt.sh
+++ scripts/wordcount_execution_inbuilt.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-INPUT_DIR=/small-log-file-analytics/input
-OUTPUT_DIR=/small-log-file-analytics/output
+# Phase 3: Distributed Processing Using Built-in MapReduce
+: '
+Task 4: Built-in WordCount Execution
+To validate distributed processing, run Hadoop’s built-in WordCount example on the log data stored in HDFS.
+Requirements:
+1. Input must be read from HDFS
+2. Output must be written to HDFS
+Analysis:
+1. Identify the number of mapper tasks launched
+2. Explain the relationship between input blocks and mapper count
+3. Observe reducer execution and shuffle behavior
+'
+
+
+INPUT_DIR=/large-log-file-analytics/input
+OUTPUT_DIR=/large-log-file-analytics/output
 
 
 # ---- CLEAN OLD OUTPUT ----
@@ -23,3 +37,19 @@ echo "Displaying sample output:"
 hdfs dfs -cat $OUTPUT_DIR/part-r-00000 | head -20
 
 echo "WordCount job completed successfully."
+
+
+# Analysis:
+# 1. Identify the number of mapper tasks launched:
+# Number of mapper tasks launched is equal to the number of blocks created. 
+echo "Number of blocks created are: "
+hdfs fsck $INPUT_DIR -files -blocks | grep "Total blocks"
+
+# 2. Explain the relationship between input blocks and mapper count
+# The relationship between input blocks and mapper count is that number of blocks directly equals number of mappers,
+# because HDFS splits input file into fixed size blocks and each block is assigned a mapper by Hadoop.
+
+# 3. Observe reducer execution and shuffle behavior
+# During execution, mapper outputs are shuffled and sorted by key across the cluster. 
+# The reducer receives grouped (word, count) pairs from all mappers and aggregates them to produce the final WordCount result.
+# The shuffle phase ensures that all values for the same key are processed by the same reducer.
